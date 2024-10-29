79c55ec694f6839967b7a7ddc84bbf7914245ce9
Ronny <ronny761@gmail.com>
2024-10-29 12:45:46 +0200

    1st upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 3; // מספר המספרים הנרקיסיסטיים לחיפוש
+        int[] narcissisticNumbers = findNarcissisticNumbers(n);
+
+        // הדפסת המספרים הנרקיסיסטיים שנמצאו
+        for (int num : narcissisticNumbers) {
+            System.out.println(num);
+        }
+    }
+
+    // פעולה למציאת n המספרים הנרקיסיסטיים הראשונים
+    public static int[] findNarcissisticNumbers(int n) {
+        // TODO: השלם את הלוגיקה למציאת המספרים הנרקיסיסטיים
+        return new int[0]; // החזרה זמנית
+    }
+
+}
